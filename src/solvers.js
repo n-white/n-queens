7a882ef1e2b17aa055c7866706db07779937106a
n-white <neil.r.whitehead@gmail.com>
2016-06-15 19:58:57 -0700

    "preliminary structure using recursion for NRooks. Returns correct solution but with all zeros on each board (it's a very hacky solution)"

--- src/solvers.js
+++ src/solvers.js
@@ -42,55 +42,51 @@ var matrixBoard = new Board({'n': n});
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  var emptyMatrixBoard = new Board({'n': n});
+window.countNRooksSolutions = function(n, currentX, currentY, currentBoard) {
+  // debugger
   var solutionCount = 0;
-  var solutionBoard = [];
- 
+  var board = currentBoard || new Board({'n': n});
+  var spotFound = false;
 
-  var helper = function(x, y, numRooks, matrixBoard) {
-    // debugger;
-    var numRooks = numRooks;
-
-    if (numRooks === 0) {
-      solutionBoard.push(matrixBoard);
-      return;
-    }
-
-    matrixBoard.rows()[y][x] = 1;
-    numRooks--;
-
-    // if matrixBoard.rows()[y][x] === 0 && no row or column conflicts
-    if (matrixBoard.hasAnyRowConflicts() || matrixBoard.hasAnyColConflicts()) {
-      matrixBoard.rows()[y][x] = 0;
-      numRooks++;
-    }
-      // place 1 at y,x
-      // decrease n--
-
-    // if x + 1 > matrixBoard.rows().length
-    if (x + 1 >= matrixBoard.rows().length) {
-      // helper(x = 0, y = y + 1, n, matrix)
-      helper(0, y+1, numRooks, matrixBoard);
-    } else {
-      // if x + 1 < matrixBoard.rows().length
-        // helperf(x = x+1, y, n, matrix) 
-      helper(x+1, y, numRooks, matrixBoard);
-    }
-
-
-  };
-
-  for ( var i = 0; i < n; i++ ) {
-    debugger;
-    emptyMatrixBoard = new Board({'n': n});
-    helper(i, 0, n, emptyMatrixBoard;
+  if ( n === 0 ) {
+    return 1;
   }
 
-  // helper(0, 0, n, emptyMatrixBoard);
+  // Use this for loop to find the next possible spot without conflicts
+  for (var y = currentY; y < board.rows().length; y++) {
+    //witin rows, iterate through columns (x)
+    for (var x = currentX; x < board.rows().length; x++) {
+      // if no conflicts at x, y: adjust matrix[y][x] == 1
+      board.rows()[y][x] = 1;
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionBoard;
+      if (board.hasAnyRowConflicts() || board.hasAnyColConflicts()) {
+        board.rows()[y][x] = 0;
+      } else {
+        spotFound = true;
+        currentY = y;
+        currentX = x;
+      }
+
+      if(spotFound) {
+        break;
+      }
+
+    }
+    if(spotFound) {
+      break;
+    }
+    currentX = 0;
+  }
+
+  console.log(board.rows());
+
+  // determine next placement of a rook given the current x and y
+  for (var i = 0; i < n; i++) {
+    solutionCount += countNRooksSolutions(n-1, currentX, currentY, board);
+  }
+
+
+  return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
@@ -110,6 +106,7 @@ window.countNQueensSolutions = function(n) {
 };
 
 
+// OLD CODE USING FOR LOOP
 
     // if (x + 1 > n) {
     //   return;
@@ -132,4 +129,52 @@ window.countNQueensSolutions = function(n) {
     // solutionCount++;
 
     // helper(x
+
+
+// OLD CODE FOR RECURSION
+
+  //   // debugger;
+  //   var numRooks = numRooks;
+
+  //   if (numRooks === 0) {
+  //     solutionBoard.push(matrixBoard);
+  //     return;
+  //   }
+
+  //   matrixBoard.rows()[y][x] = 1;
+  //   numRooks--;
+
+  //   // if matrixBoard.rows()[y][x] === 0 && no row or column conflicts
+  //   if (matrixBoard.hasAnyRowConflicts() || matrixBoard.hasAnyColConflicts()) {
+  //     matrixBoard.rows()[y][x] = 0;
+  //     numRooks++;
+  //   }
+  //     // place 1 at y,x
+  //     // decrease n--
+
+  //   // if x + 1 > matrixBoard.rows().length
+  //   if (x + 1 >= matrixBoard.rows().length) {
+  //     // helper(x = 0, y = y + 1, n, matrix)
+  //     helper(0, y+1, numRooks, matrixBoard);
+  //   } else {
+  //     // if x + 1 < matrixBoard.rows().length
+  //       // helperf(x = x+1, y, n, matrix) 
+  //     helper(x+1, y, numRooks, matrixBoard);
+  //   }
+
+
+  // };
+
+  // for ( var i = 0; i < n; i++ ) {
+  //   // debugger;
+  //   emptyMatrixBoard = new Board({'n': n});
+  //   helper(i, 0, n, emptyMatrixBoard);
+  // }
+
+  // // helper(0, 0, n, emptyMatrixBoard);
+
+  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  // return solutionBoard.length;
+
+
 
